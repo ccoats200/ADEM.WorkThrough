71ef4f45f8337ab3ca10aff91d9b10bfec519acd
ccoats200 <colemancoats@colemans-macbook-pro.local>
2018-02-02 17:32:52 -0500

    Initial Commit

---
+++ ADEM/ListVC.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ADEM
+//
+//  Created by Coleman Coats on 1/25/18.
+//  Copyright © 2018 Coleman Coats. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+	
+	//Check out will add a check mark to all items in the list, If you dont want on you uncheck it. at the same time the bottom will raise up to reveal a apple pay logo and the store you want to buy it from. Because you are loged into your rewards program you get the deals and the cash back immediatly.
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	func 
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ ADEM/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  ADEM
+//
+//  Created by Coleman Coats on 1/30/18.
+//  Copyright © 2018 Coleman Coats. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+	@IBOutlet weak var itemImage: UIImageView!
+	
+	@IBOutlet weak var productName: UILabel!
+	@IBOutlet weak var calCount: UILabel!
+	@IBOutlet weak var productPrice: UILabel!
+	
+}
