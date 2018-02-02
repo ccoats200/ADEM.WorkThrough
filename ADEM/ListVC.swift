fad652983b42bf171a2d39b8d7a42ce8a8deab41
ccoats200 <colemancoats@colemans-macbook-pro.local>
2018-02-02 17:52:49 -0500

    why

--- ADEM/ListVC.swift
+++ ADEM/ListVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ListVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-
+	
 	
 	//Check out will add a check mark to all items in the list, If you dont want on you uncheck it. at the same time the bottom will raise up to reveal a apple pay logo and the store you want to buy it from. Because you are loged into your rewards program you get the deals and the cash back immediatly.
 	
